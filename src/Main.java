a714caed98be36098692b9ca6f83dd0ce1cface0
Zuza <zwarminska@gmail.com>
2021-11-07 12:58:25 +0100

    Hello world is my first program

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int liczba = 5;
+        int liczba2 = 2;
+        System.out.print("Hello world"+liczba);
+
+        System.out.println(liczba%liczba2);
+
+        String name = "Zuza ";
+        int age = 30;
+        System.out.println("My name is " + name + "and i am "+ age);
+
     }
 
 }
