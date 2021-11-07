c0938dce10a08bf481bb6bf49e87f02f774ef722
Zuza <zwarminska@gmail.com>
2021-11-07 15:03:23 +0100

    Hello world is my first program

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,55 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int liczba = 5;
-        int liczba2 = 2;
-//        System.out.print("Hello world"+liczba);
+//        int liczba = 5;
+//        int liczba2 = 2;
+////        System.out.print("Hello world"+liczba);
+////
+////        System.out.println(liczba%liczba2);
+////
+////        String name = "Zuza ";
+////        int age = 30;
+////        System.out.println("My name is " + name + "and i am "+ age);
+////
+////
+////
+//        String account = " ZuzaWarminska ";
+//        account.length();
+//        System.out.println(account.length());
 //
-//        System.out.println(liczba%liczba2);
+//        System.out.println(account.indexOf('r'));
 //
-//        String name = "Zuza ";
-//        int age = 30;
-//        System.out.println("My name is " + name + "and i am "+ age);
+//        System.out.println(account.substring(0,5));
 //
+//        System.out.println(account.trim());
 //
+//        System.out.println(account.length());
 //
-        String account = " ZuzaWarminska ";
-        account.length();
-        System.out.println(account.length());
+//        char a = 'a';
+//        char alpha = '\u03B1';
+//        System.out.println(alpha);
+//
+//        char[] witaj = {'w','i','t','a','j'};
+//        System.out.println(witaj);
+//        Character.isDigit(a);
+//        System.out.println(Character.isDigit(a));
+//
+//        if (liczba>liczba2 | liczba2==liczba){
+//            System.out.println("program_dziala");
+//        }else{
+//            System.out.println("program_niedziala");
+//            if (liczba==liczba2){
+//                System.out.println("system_dziala");
+//            }
+//        }
+// }
+        Scanner skaner = new Scanner(System.in);
+        System.out.println("Podaj imie");
+        String userInputName = skaner.nextLine();
+        System.out.println("Podaj nazwisko");
+        String userInputSurname = skaner.nextLine();
+        System.out.println("Witaj" + userInputName + " " + userInputSurname +"!");
 
-        System.out.println(account.indexOf('r'));
 
-        System.out.println(account.substring(0,5));
-
-        System.out.println(account.trim());
-
-        System.out.println(account.length());
-
-        char a = 'a';
-        char alpha = '\u03B1';
-        System.out.println(alpha);
-
-        char[] witaj = {'w','i','t','a','j'};
-        System.out.println(witaj);
-        Character.isDigit(a);
-        System.out.println(Character.isDigit(a));
-
-        if (liczba>liczba2 | liczba2==liczba){
-            System.out.println("program_dziala");
-        }else{
-            System.out.println("program_niedziala");
-            if (liczba==liczba2){
-                System.out.println("system_dziala");
-            }
-        }
     }
-
 }
