b1ec87f67a54f7085e68f3ccc2c69d8256f18f0f
Zuza <zwarminska@gmail.com>
2021-11-20 10:00:40 +0100

    Liczby niedokończone

--- src/Main.java
+++ src/Main.java
@@ -1,75 +1,11 @@
-import java.util.ArrayList;
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-//        int liczba = 5;
-//        int liczba2 = 2;
-////        System.out.print("Hello world"+liczba);
-////
-////        System.out.println(liczba%liczba2);
-////
-////        String name = "Zuza ";
-////        int age = 30;
-////        System.out.println("My name is " + name + "and i am "+ age);
-////
-////
-////
-//        String account = " ZuzaWarminska ";
-//        account.length();
-//        System.out.println(account.length());
-//
-//        System.out.println(account.indexOf('r'));
-//
-//        System.out.println(account.substring(0,5));
-//
-//        System.out.println(account.trim());
-//
-//        System.out.println(account.length());
-//
-//        char a = 'a';
-//        char alpha = '\u03B1';
-//        System.out.println(alpha);
-//
-//        char[] witaj = {'w','i','t','a','j'};
-//        System.out.println(witaj);
-//        Character.isDigit(a);
-//        System.out.println(Character.isDigit(a));
-//
-//        if (liczba>liczba2 | liczba2==liczba){
-//            System.out.println("program_dziala");
-//        }else{
-//            System.out.println("program_niedziala");
-//            if (liczba==liczba2){
-//                System.out.println("system_dziala");
-//            }
-//        }
-// }
-//        Scanner skaner = new Scanner(System.in);
-//        System.out.println("Podaj imie");
-//        String userInputName = skaner.nextLine();
-//        System.out.println("Podaj nazwisko");
-//        String userInputSurname = skaner.nextLine();
-//        System.out.println("Witaj" + userInputName + " " + userInputSurname +"!");
-//
-//    int number = 3;
-//    int number2;
-//        System.out.println(number++);
-//        System.out.println(++number);
-//        System.out.println(number);
-//        number2 = number++;
-//        System.out.println(number2);
-//        number2 = ++number;
-//        System.out.println(number2);
-//        System.out.println(++number2);
-
-
-        ArrayList<String> lista = new ArrayList<>();
-  String[] nameTable = new String[4];
-  String[] nameTableBio = {"kasia","zosia","tomasz","asia"};
-  for (int i = 0; i < nameTableBio.length; i++) {
-      nameTable.
-  }
-
-
+        int [] tablica = {6, 5, 4, 3, 2, 1};
+        for(int i=0; i< tablica.length; i++){
+            if (tablica[i] < 3){
+                System.out.println("True");
+            }else if(tablica[i] < 5){
+                System.out.println("False");}
+        }
     }
 }
